6af5cfa9143e7fe005f2462f0433c0ce4ee5bab1
japint <jap1centeno@gmail.com>
2024-05-14 18:26:17 +0200

    Added Feature Rule button functionality

---
+++ main.js
@@ -0,0 +1,16 @@
+/* Start of rule-btn */
+function sayHello() {
+    console.log("rules was toggled!");
+}
+const open = document.getElementById('open')
+const popup_container = document.getElementById('popup_container')
+const close = document.getElementById('close')
+
+open.addEventListener('click', () => {
+    popup_container.classList.add('show');
+});
+
+close.addEventListener('click', () => {
+    popup_container.classList.remove('show');
+});
+/* End of rule-btn */
